749b19cea81ff89b2c3b32f8c55ea8cd0bf6c538
Ibrahim Elkhalil Benyahia <ibdev@ibdev>
2024-07-15 13:39:18 -0400

    add some comments to users route

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -5,7 +5,7 @@ import uuidValidator from "../helpers/validation/uuidValidator.js";
 
 const router = Router();
 
-/**
+/************************************************************************************
  * ?-[] get user information
  * ?-[] update user information
  * ?-[] delete user
@@ -40,7 +40,7 @@ router.get('/', isAuthenticated,async (req, res) => {
         return res.status(500).json({msg:err || 'unknown server error'})
     }
 })
-/**
+/*********************************************************************************
  * !-url= http://localhost:5000/api/users/send
  * !-Method= POST
  * !-Midlewares= isAuthenticated : /midlewares/isAuthenticated.js|
@@ -52,13 +52,19 @@ router.get('/', isAuthenticated,async (req, res) => {
  * ?             | 500:[INTERNAL ERROR]=>{msg:err}
  */
 router.patch('/send', isAuthenticated,async (req, res) => {
+    //* curent user id
     const {user_id:id} = req;
+    //* receiver user id
     const receiver_id = req.body.rid;
+    //* check receiver user id to be uuid to protect
     if(!uuidValidator(receiver_id) || id === receiver_id) return res.status(400).json({msg:'invalid data'})
     try {
+        //* make sure that the reciever does not received previous friend request and is not already a friend
         const ruser = await prisma.user.findUnique({where:{id:receiver_id, friendof:{ none:{id} } } });
         if(!ruser) return res.status(404).json({msg:"can not send freind request to this user"});
+        //* updating curent user to add the receiver to his friends
         await prisma.user.update({where:{id} , data:{firends:{connect:{id:ruser.id} } } });
+        //* returning status of 200 and success message
         return res.status(200).json({msg:'friend request sent'})
     }
 
@@ -66,7 +72,7 @@ router.patch('/send', isAuthenticated,async (req, res) => {
         return res.status(500).json({msg:err || 'unknowm server error'})
     }
 })
-/**
+/************************************************************************************
  * !-url= http://localhost:5000/api/users/accept
  * !-Method= POST
  * !-Midlewares= isAuthenticated : /midlewares/isAuthenticated.js|
@@ -78,17 +84,24 @@ router.patch('/send', isAuthenticated,async (req, res) => {
  * ?             | 500:[INTERNAL ERROR]=>{msg:err}
  */
 router.patch('/accept', isAuthenticated,async (req, res) => {
+    //* curent user id
     const {user_id:id} = req;
+    //* the id of the user who sent a freind request 
     const sender_id = req.body.sid;
+    //* validating the id with function to protect
     if(!uuidValidator(sender_id) || id === sender_id) return res.status(400).json({msg:'invalid data'})
     try {
-        const suser = await prisma.user.findUnique({where:{id:sender_id, friendof:{ some:{id} }, firends:{none: {id} } } });
+        //* get sender user and make sure that is not already a friend and make sure that is send a request to the current user
+        const suser = await prisma.user.findUnique({where:{id:sender_id, friendof:{ none:{id} }, firends:{some: {id} } } });
         if(!suser) return res.status(404).json({msg:"can not accept freind request to this user"});
-        await prisma.user.update({where:{id} , data:{firends:{connect:{id:suser.id} } } });
+        //* updating the sender user and make the current user a friend to him (accepting process)
+        await prisma.user.update({where:{id:sender_id} , data:{friendof:{connect:{id} } } });
+        //* returning a status of 200 of success
         return res.status(200).json({msg:'friend request accepted'})
     }
 
     catch(err){
+        //* hundling unexprected errors
         return res.status(500).json({msg:err || 'unknowm server error'})
     }
 })
