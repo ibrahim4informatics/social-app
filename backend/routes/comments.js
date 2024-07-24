29754dd9786aa843559590f08639197aac6d07a7
Ibrahim Elkhalil Benyahia <ibdev@ibdev>
2024-07-24 11:56:20 -0400

    finish the comment router

--- backend/routes/comments.js
+++ backend/routes/comments.js
@@ -39,29 +39,56 @@ router.post('/', async (req, res) => {
 });
 /************************************************************************************
  * !-url= http://localhost:5000/api/users/:id
- * !-Method= GET
+ * !-Method= PATCH
  * !-Midlewares= isAuthenticated : /midlewares/isAuthenticated.js|
  * * -PARAMS id:UUID
- * * -BODY= NULL
+ * * -BODY= {content}
  * * -QEURY= NULL
- * ? -RESPONSE=  200:[OK] => {user:{}}
- * ?             | 500:[INTERNAL ERROR]=>{msg:err}
  */
 router.patch('/:id', async (req, res) => {
-    res.sendStatus(200)
+
+    const { user_id } = req;
+    const { id } = req.params;
+    const { content, post_id } = req.body;
+    if (!uuidValidator(post_id) || !uuidValidator(id)) return res.status(400).json({ msg: "invalid params passed" });
+    const criteriaComment = { id, user_id, post_id };
+    try {
+
+        const user = await prisma.user.findUnique({ where: { id: user_id } });
+        if (!user) return res.status(403).json({ msg: "can not perform this action" });
+        const comment = await prisma.comment.findUnique({ where: criteriaComment });
+        if (!comment) return res.status(404).json({ msg: "can not find comment" });
+
+        await prisma.comment.update({ where: criteriaComment, data: { content: domPurify.sanitize(content) } });
+        return res.status(200).json({ msg: "comment update suceess" });
+
+    }
+    catch (err) {
+        return res.status(500).json({ msg: err || "unknown server error" });
+    }
 });
 /************************************************************************************
- * !-url= http://localhost:5000/api/users/:id
- * !-Method= GET
+ * !-url= http://localhost:5000/api/comments/:id
+ * !-Method= DELETE
  * !-Midlewares= isAuthenticated : /midlewares/isAuthenticated.js|
  * * -PARAMS id:UUID
  * * -BODY= NULL
  * * -QEURY= NULL
- * ? -RESPONSE=  200:[OK] => {user:{}}
- * ?             | 500:[INTERNAL ERROR]=>{msg:err}
  */
 router.delete('/:id', async (req, res) => {
-    res.sendStatus(200)
+    const { user_id } = req;
+    const { id } = req.params;
+    if (!uuidValidator(id)) return res.status(400).json({ msg: "invalid url parameter" });
+    const commentCrieria = { id: domPurify.sanitize(id), user_id: domPurify.sanitize(user_id) }
+    try {
+        const comment = await prisma.comment.findUnique({ where: commentCrieria });
+        if (!comment) return res.status(404).json({ msg: "can not find this comment" });
+        await prisma.comment.delete({ where: commentCrieria });
+        return res.status(200).json({ msg: "comment delete success" });
+    }
+    catch (err) {
+        return res.sendStatus(500).json({ msg: err || "unknown server error" });
+    };
 });
 
 export default router;
